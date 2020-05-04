b300678a82b77c3802cdc769981970d74d0b9add
Damian <damian.ogorek.1996@gmail.com>
2020-05-04 21:32:33 +0200

    Added Unit test for the method and fixed bugs.

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -1,13 +1,13 @@
 package com.example.android.architecture.blueprints.todoapp.statistics
 
 import com.example.android.architecture.blueprints.todoapp.data.Task
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 /**
  * Created by Damian on 04.05.2020 21:10
  */
-class StatisticsUtilsTest{
+class StatisticsUtilsTest {
 
     //One active task.
     //There are 100% percentage active tasks and 0% completed tasks.
@@ -45,5 +45,36 @@ class StatisticsUtilsTest{
 
         assertEquals(result.completedTasksPercent, 40f)
         assertEquals(result.activeTasksPercent, 60f)
+    }
+
+
+    //Empty list of active and completed tasks.
+    //There are 0% percentage active tasks and 0% completed tasks.
+    @Test
+    fun getActiveAndCompletedStats_emptyList_returnsHundredZero() {
+
+        //Create and active tasks (the false make this active)
+        val tasks = emptyList<Task>()
+
+        //Call function to get result
+        val result = getActiveAndCompletedStats(tasks)
+
+        assertEquals(result.completedTasksPercent, 0f)
+        assertEquals(result.activeTasksPercent, 0f)
+    }
+
+    //Null list of active and completed tasks.
+    //There are 0% percentage active tasks and 0% completed tasks.
+    @Test
+    fun getActiveAndCompletedStats_null_returnsHundredZero() {
+
+        //Create and active tasks (the false make this active)
+        val tasks = null
+
+        //Call function to get result
+        val result = getActiveAndCompletedStats(tasks)
+
+        assertEquals(result.completedTasksPercent, 0f)
+        assertEquals(result.activeTasksPercent, 0f)
     }
 }
