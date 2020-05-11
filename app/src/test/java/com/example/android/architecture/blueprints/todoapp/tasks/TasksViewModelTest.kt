85baf9ae475e4f06e42ac7fe56856261860df054
Damian <damian.ogorek.1996@gmail.com>
2020-05-11 21:25:38 +0200

    Used the fake repository inside a ViewModel.

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -3,33 +3,40 @@ package com.example.android.architecture.blueprints.todoapp.tasks
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
-
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.android.architecture.blueprints.todoapp.Event
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeTestRepository
 import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
 
 /**
  * Created by Damian on 05.05.2020 21:38
  */
 
-@RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
 
+    private lateinit var tasksRepository: FakeTestRepository
+
     // Subject under test
-    private lateinit var taskViewModel: TasksViewModel
+    private lateinit var tasksViewModel: TasksViewModel
 
     // Given a fresh ViewModel
     @Before
     fun setupVieModel() {
-        taskViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+
+        // Initialise the tasks to 3, with one active and two completed
+        tasksRepository = FakeTestRepository()
+        val task1 = Task("Title1", "Description1")
+        val task2 = Task("Title2", "Description2", true)
+        val task3 = Task("Title3", "Description3", true)
+        tasksRepository.addTasks(task1, task2, task3)
+
+        tasksViewModel = TasksViewModel(tasksRepository)
     }
 
     // Executes each task synchronously using Architecture Components.
@@ -45,16 +52,16 @@ class TasksViewModelTest {
         try {
 
             //Observe LiveData forever
-            taskViewModel.newTaskEvent.observeForever(observer)
+            tasksViewModel.newTaskEvent.observeForever(observer)
 
             // When adding a new task
-            taskViewModel.addNewTask()
+            tasksViewModel.addNewTask()
 
             // Then the new task event is triggered
-            val value = taskViewModel.newTaskEvent.value
+            val value = tasksViewModel.newTaskEvent.value
             assertThat(value?.getContentIfNotHandled(), (not(nullValue())))
         } finally {
-            taskViewModel.newTaskEvent.removeObserver(observer)
+            tasksViewModel.newTaskEvent.removeObserver(observer)
         }
     }
 
@@ -62,10 +69,10 @@ class TasksViewModelTest {
     fun addNewTask_setsNewTaskEvent() {
 
         // When adding a new task
-        taskViewModel.addNewTask()
+        tasksViewModel.addNewTask()
 
         // Then the new task event is triggered
-        val value = taskViewModel.newTaskEvent.getOrAwaitValue()
+        val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
         assertThat(value.getContentIfNotHandled(), (not(nullValue())))
     }
 
@@ -73,10 +80,10 @@ class TasksViewModelTest {
     fun setFilterAllTasks_tasksAddViewVisible() {
 
         // When the filter type is ALL_TASKS
-        taskViewModel.setFiltering(TasksFilterType.ALL_TASKS)
+        tasksViewModel.setFiltering(TasksFilterType.ALL_TASKS)
 
         // Then the "Add task" action is visible
-        val value = taskViewModel.tasksAddViewVisible.getOrAwaitValue()
+        val value = tasksViewModel.tasksAddViewVisible.getOrAwaitValue()
         assertThat(value, `is`(true))
     }
 }
