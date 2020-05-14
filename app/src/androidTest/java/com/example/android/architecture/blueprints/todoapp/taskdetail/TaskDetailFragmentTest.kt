ccd8b216cc1ad8b006d54f71e72fb3de794b1ed7
Damian <damian.ogorek.1996@gmail.com>
2020-05-14 20:52:53 +0200

    Used service locator in tests

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
@@ -4,7 +4,14 @@ import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.example.android.architecture.blueprints.todoapp.R
+import com.example.android.architecture.blueprints.todoapp.ServiceLocator
 import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeAndroidTestRepository
+import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -14,12 +21,28 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @MediumTest
+@ExperimentalCoroutinesApi
 class TaskDetailFragmentTest {
 
+    private lateinit var tasksRepository: TasksRepository
+
+    @Before
+    fun initRepository() {
+        tasksRepository = FakeAndroidTestRepository()
+        ServiceLocator.tasksRepository = tasksRepository
+    }
+
+    @After
+    fun cleanupDatabase() = runBlockingTest {
+        ServiceLocator.resetRepository()
+    }
+
     @Test
-    fun activeTaskDetail_DisplayedInUi() {
+    fun activeTaskDetail_DisplayedInUi() = runBlockingTest {
         //GIVEN - Add active (incomplete) task to the DB
         val activeTask = Task(" Active task", "AndroidX Rocks!!", false)
+
+        tasksRepository.saveTask(activeTask)
 
         //WHEN - Details fragment launched to display task
         val bundle = TaskDetailFragmentArgs(activeTask.id).toBundle()
